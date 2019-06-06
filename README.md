daf6baef2ba8f8c8b2a5b680a0c647a2f1e62c79
dceoy <dnarsil+github@gmail.com>
2019-06-07 04:27:11 +0900

    Add command.R

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# command-template-r
-Tiny and portable R script as a shell command
+command-template-r
+==================
+
+Tiny R script as an executable command
+
+- R script scaffold for command-line tool development
+  - Default options
+    - `--debug`: Run with debug logging
+    - `--seed`: Set a random seed
+    - `--cpus`: Limit CPU cores for multithreading
+    - `--in`: Set an input directory
+    - `--out`: Set an output directory
+    - `--version`: Print version and exit
+    - `-h, --help`: Print help and exit
+
+Usage
+-----
+
+1.  Install [docopt.R](https://github.com/docopt/docopt.R).
+
+    ```sh
+    $ R -e 'install.packages(pkgs = "docopt", dependencies = TRUE, repos = "https://cran.rstudio.com/");'
+    ```
+
+2.  Download `command.R`.
+
+    ```sh
+    $ curl -SLO https://raw.githubusercontent.com/dceoy/command-template-r/master/command.R
+    $ chmod +x command.R
+    ```
+
+3.  Test `command.R`.
+
+    ```sh
+    $ ./command.R --help
+    ```
+
+    Expected output:
+
+    ```
+    Tiny R script as an executable command
+
+    Usage:
+      command.R [--debug] [--seed=<int>] [--cpus=<int>] [--in=<dir>] [--out=<dir>]
+      command.R --version
+      command.R -h|--help
+
+    Options:
+      --debug         Run with debug logging
+      --seed=<int>    Set a random seed
+      --cpus=<int>    Limit CPU cores for multithreading
+      --in=<dir>      Set an input directory [default: .]
+      --out=<dir>     Set an output directory [default: .]
+      --version       Print version and exit
+      -h, --help      Print help and exit
+    ```
+
+4.  Rename and edit the script.
+
+    ```sh
+    $ mv command.R your_command.R
+    $ vim your_command.R            # => edit
+    ```
---
+++ command.R
@@ -0,0 +1,85 @@
+#!/usr/bin/env Rscript
+
+'Tiny R script as an executable command
+
+Usage:
+  command.R [--debug] [--seed=<int>] [--cpus=<int>] [--in=<dir>] [--out=<dir>]
+  command.R --version
+  command.R -h|--help
+
+Options:
+  --debug         Run with debug logging
+  --seed=<int>    Set a random seed
+  --cpus=<int>    Limit CPU cores for multithreading
+  --in=<dir>      Set an input directory [default: .]
+  --out=<dir>     Set an output directory [default: .]
+  --version       Print version and exit
+  -h, --help      Print help and exit' -> doc
+
+
+script_version <- 'v0.0.1'
+
+
+fetch_script_root <- function() {
+  ca <- commandArgs(trailingOnly = FALSE)
+  fa <- ca[grepl('^--file=', ca)]
+  if (length(fa) == 1) {
+    f <- sub('--file=', '', fa)
+    l <- Sys.readlink(f)
+    if (is.na(l)) {
+      script_path <- normalizePath(f)
+    } else if (startsWith(l, '/')) {
+      script_path <- normalizePath(l)
+    } else {
+      script_path <- normalizePath(file.path(dirname(f), l))
+    }
+    return(dirname(script_path))
+  } else {
+    return(normalizePath(getwd()))
+  }
+}
+
+
+main <- function(opts, pkgs = NULL, rscripts = NULL,
+                 root_dir = fetch_script_root()) {
+  options(warn = 1)
+  if (opts[['--debug']]) print(opts)
+
+  if (! is.null(opts[['--seed']])) {
+    message('>>> Set a random seed')
+    set.seed(opts[['--seed']])
+    message(opts[['--seed']])
+  }
+
+  if (! is.null(pkgs)) {
+    message('>>> Load packages')
+    print(suppressMessages(sapply(all_pkgs, require, character.only = TRUE)))
+  }
+
+  if (! is.null(rscripts)) {
+    message('>>> Load scripts')
+    lapply(file.path(root_dir, rscripts), source)
+  }
+
+  n_cpu <- ifelse(is.null(opts[['--cpus']]),
+                  parallel::detectCores(), as.integer(opts[['--cpus']]))
+  in_dir <- normalizePath(opts[['--in']])
+  out_dir <- normalizePath(opts[['--out']])
+
+  message('>>> Show configurations')
+  message('Script directory path:       ', root_dir)
+  message('Input directory path:        ', in_dir)
+  message('Output directory path:       ', out_dir)
+  message('CPU cores:                   ', n_cpu)
+
+  #
+  # message('>>> Start the anaysis workflow')
+  # start_analysis()
+  #
+}
+
+
+if (! interactive()) {
+  require('docopt', quietly = TRUE)
+  main(opts = docopt::docopt(doc, version = script_version))
+}
